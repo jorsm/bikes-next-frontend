0ee5b407b29c8296931d356c487404c859496230
jorsm <jan.oradini@studenti.unitn.it>
2022-09-01 17:35:53 +0200

    updated next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const api_url =
   process?.env?.NODE_ENV == "production"
-    ? "https://bikes-ledro.herokuapp.com"
+    ? "https://bikes-ledro.herokuapp.com/api/v1"
     : "http://192.168.1.102:5000/api/v1";
 
 const nextConfig = {
